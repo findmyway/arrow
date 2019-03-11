eeb91ae7a0b83e7533052759aeae4ce7e9e50d62
Jun Tian <juti@microsoft.com>
2019-03-11 12:20:24 +0800

    add OjbectID

--- cpp/src/plasma/lib/julia/plasma_wrapper.cc
+++ cpp/src/plasma/lib/julia/plasma_wrapper.cc
@@ -8,6 +8,16 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
     mod.add_type<arrow::Buffer>("Buffer");
     mod.add_type<arrow::Status>("Status");
 
+    mod.add_type<plasma::UniqueID>("ObjectID")
+        .method("data", &plasma::UniqueID::data)
+        .method("mutable_data", &plasma::UniqueID::mutable_data)
+        .method("binary", &plasma::UniqueID::binary)
+        .method("hex", &plasma::UniqueID::hex)
+        .method("hash", &plasma::UniqueID::hex)
+        .method("size", &plasma::UniqueID::size);
+
+    mod.method("from_binary", &plasma::UniqueID::from_binary);
+
     mod.add_type<plasma::PlasmaClient>("PlasmaClient")
         .constructor<>()
         .method("connect", &plasma::PlasmaClient::Connect)
